b63ebd0bbe235d352888acdfc0d74d511a318290
Igor <igorwho@yandex.ru>
2025-09-02 01:00:30 +0300

    implement LRU cache

--- src/lru_cache.rs
+++ src/lru_cache.rs
@@ -1,10 +1,12 @@
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
+use std::iter::FusedIterator;
+use std::marker::PhantomData;
 use std::mem::MaybeUninit;
 use std::num::NonZeroUsize;
-use std::{mem, ptr};
-use std::borrow::Borrow;
-use std::ptr::{drop_in_place, NonNull};
+use std::ptr::{NonNull, drop_in_place};
+use std::{fmt, mem, ptr};
 
 struct KeyRef<K> {
     k: *const K,
@@ -24,8 +26,8 @@ impl<K: PartialEq> PartialEq for KeyRef<K> {
 
 impl<K: Eq> Eq for KeyRef<K> {}
 
-// This type exists to allow a "blanket" Borrow impl for KeyRef without conflicting with the
-//  stdlib blanket impl
+// This type exists to allow a "blanket" Borrow implementation for KeyRef
+// without conflicting with the stdlib blanket implementation.
 #[repr(transparent)]
 struct KeyWrapper<K: ?Sized>(K);
 
@@ -64,7 +66,7 @@ where
 struct Node<K, V> {
     key: MaybeUninit<K>,
     val: MaybeUninit<V>,
-    // prev<--node-->next
+    // prev <-- node --> next
     prev: *mut Node<K, V>,
     next: *mut Node<K, V>,
 }
@@ -96,7 +98,7 @@ struct LruCache<K, V> {
     tail: *mut Node<K, V>,
 }
 
-impl<K: Eq + Hash, V> LruCache<K, V> {
+impl<K: Hash + Eq, V> LruCache<K, V> {
     pub fn new(cap: NonZeroUsize) -> LruCache<K, V> {
         let cache = LruCache {
             map: HashMap::with_capacity(cap.get()),
@@ -118,9 +120,9 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
     }
 
     // Pushes a key-value pair into the cache. If an entry with key `k` already exists in
-    // the cache or another cache entry is removed (due to the LRU's capacity),
+    // the cache or another cache entry is removed (due to the LRU capacity),
     // then it returns the old entry's key-value pair. Otherwise, returns `None`.
-    pub fn push(&mut self, k: K, v: V) -> Option<(K ,V)> {
+    pub fn push(&mut self, k: K, v: V) -> Option<(K, V)> {
         self.capturing_put(k, v, true)
     }
 
@@ -175,7 +177,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
             // old node is with updated key and value
             (Some(replaced_kv), old_node)
         } else {
-            // if the cache is not full allocate a new Node
+            // if the cache is not full allocate a new Node.
             // Safety: We allocate, turn into raw, and get NonNull all in one step.
             (None, unsafe {
                 NonNull::new_unchecked(Box::into_raw(Box::new(Node::new(k, v))))
@@ -225,12 +227,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
         if let Some(node) = self.map.get_mut(KeyWrapper::from_ref(k)) {
             let node_ptr = node.as_ptr();
             self.move_to_front(node_ptr);
-            Some(unsafe {
-                (
-                    &*(*node_ptr).key.as_ptr(),
-                    &*(*node_ptr).val.as_ptr()
-                )
-            })
+            Some(unsafe { (&*(*node_ptr).key.as_ptr(), &*(*node_ptr).val.as_ptr()) })
         } else {
             None
         }
@@ -266,7 +263,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
     where
         F: FnOnce() -> V,
     {
-        if let Some(node) = self.map.get_mut(&KeyRef{ k: &k }) {
+        if let Some(node) = self.map.get_mut(&KeyRef { k: &k }) {
             let node_ptr = node.as_ptr();
             self.move_to_front(node_ptr);
             unsafe { &*(*node_ptr).val.as_ptr() }
@@ -321,7 +318,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
     where
         F: FnOnce() -> Result<V, E>,
     {
-        if let Some(node) = self.map.get_mut(&KeyRef{ k: &k }) {
+        if let Some(node) = self.map.get_mut(&KeyRef { k: &k }) {
             let node_ptr = node.as_ptr();
             self.move_to_front(node_ptr);
             Ok(unsafe { &*(*node_ptr).val.as_ptr() })
@@ -376,7 +373,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
     where
         F: FnOnce() -> V,
     {
-        if let Some(node) = self.map.get_mut(&KeyRef{ k: &k }) {
+        if let Some(node) = self.map.get_mut(&KeyRef { k: &k }) {
             let node_ptr = node.as_ptr();
             self.move_to_front(node_ptr);
             unsafe { &mut *(*node_ptr).val.as_mut_ptr() }
@@ -430,7 +427,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
     where
         F: FnOnce() -> Result<V, E>,
     {
-        if let Some(node) = self.map.get_mut(&KeyRef{ k: &k }) {
+        if let Some(node) = self.map.get_mut(&KeyRef { k: &k }) {
             let node_ptr = node.as_ptr();
             self.move_to_front(node_ptr);
             Ok(unsafe { &mut *(*node_ptr).val.as_mut_ptr() })
@@ -544,17 +541,15 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
         K: Borrow<Q>,
         Q: Hash + Eq + ?Sized,
     {
-        self.map
-            .remove(KeyWrapper::from_ref(k))
-            .map(|old_node| {
-                let mut old_node = unsafe {
-                    let mut old_node = *Box::from_raw(old_node.as_ptr());
-                    drop_in_place(old_node.key.as_mut_ptr());
-                    old_node
-                };
-                self.detach(&mut old_node);
-                unsafe { old_node.val.assume_init() }
-            })
+        self.map.remove(KeyWrapper::from_ref(k)).map(|old_node| {
+            let mut old_node = unsafe {
+                let mut old_node = *Box::from_raw(old_node.as_ptr());
+                drop_in_place(old_node.key.as_mut_ptr());
+                old_node
+            };
+            self.detach(&mut old_node);
+            unsafe { old_node.val.assume_init() }
+        })
     }
 
     // Removes and returns the key and the value corresponding to the key from the cache or
@@ -564,14 +559,12 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
         K: Borrow<Q>,
         Q: Hash + Eq + ?Sized,
     {
-        self.map
-            .remove(KeyWrapper::from_ref(k))
-            .map(|old_node| {
-                let mut old_node = unsafe { *Box::from_raw(old_node.as_ptr()) };
-                self.detach(&mut old_node);
-                let Node { key, val, .. } = old_node;
-                unsafe { (key.assume_init(), val.assume_init()) }
-            })
+        self.map.remove(KeyWrapper::from_ref(k)).map(|old_node| {
+            let mut old_node = unsafe { *Box::from_raw(old_node.as_ptr()) };
+            self.detach(&mut old_node);
+            let Node { key, val, .. } = old_node;
+            unsafe { (key.assume_init(), val.assume_init()) }
+        })
     }
 
     // Removes and returns the key and value corresponding to the least recently
@@ -622,12 +615,11 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
         false
     }
 
-
     fn remove_first(&mut self) -> Option<Box<Node<K, V>>> {
         let next = unsafe { (*self.head).next };
         if !ptr::eq(next, self.tail) {
             let old_key = KeyRef {
-                k: unsafe { &*(*next).key.as_ptr() }
+                k: unsafe { &*(*next).key.as_ptr() },
             };
             let old_node = self.map.remove(&old_key).unwrap();
             let old_node_ptr = old_node.as_ptr();
@@ -642,7 +634,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
         let prev = unsafe { (*self.tail).prev };
         if !ptr::eq(prev, self.head) {
             let old_key = KeyRef {
-                k: unsafe { &*(*prev).key.as_ptr() }
+                k: unsafe { &*(*prev).key.as_ptr() },
             };
             let old_node = self.map.remove(&old_key).unwrap();
             let old_node_ptr = old_node.as_ptr();
@@ -658,7 +650,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
         self.attach(node);
     }
 
-    // removes node
+    // Removes node.
     fn detach(&mut self, node: *mut Node<K, V>) {
         unsafe {
             (*(*node).prev).next = (*node).next;
@@ -666,7 +658,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
         }
     }
 
-    // attaches node after head node
+    // Attaches node after head node.
     fn attach(&mut self, node: *mut Node<K, V>) {
         unsafe {
             (*node).prev = self.head;
@@ -676,7 +668,7 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
         }
     }
 
-    // attaches node before tail node
+    // Attaches node before tail node.
     fn attach_last(&mut self, node: *mut Node<K, V>) {
         unsafe {
             (*node).prev = (*self.tail).prev;
@@ -714,15 +706,254 @@ impl<K: Eq + Hash, V> LruCache<K, V> {
         self.map.contains_key(KeyWrapper::from_ref(k))
     }
 
-    pub fn len(&self) -> usize {
-        self.map.len()
-    }
-
     pub fn is_empty(&self) -> bool {
         self.map.len() == 0
     }
 
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
+
     pub fn cap(&self) -> NonZeroUsize {
         self.cap
+    }
+
+    // An iterator visiting all entries in most-recently used order.
+    // The iterator element type is `(&K, &V)`.
+    pub fn iter(&self) -> Iter<K, V> {
+        Iter {
+            ptr: unsafe { (*self.head).next },
+            end: unsafe { (*self.tail).prev },
+            len: self.len(),
+            _phantom: PhantomData,
+        }
+    }
+
+    // An iterator visiting all entries in most-recently-used order,
+    // giving a mutable reference to V.
+    // The iterator element type is `(&K, &mut V)`.
+    pub fn iter_mut(&self) -> IterMut<K, V> {
+        IterMut {
+            ptr: unsafe { (*self.head).next },
+            end: unsafe { (*self.tail).prev },
+            len: self.len(),
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<K, V> Drop for LruCache<K, V> {
+    fn drop(&mut self) {
+        self.map.drain().for_each(|(_, node)| unsafe {
+            let mut node = *Box::from_raw(node.as_ptr());
+            drop_in_place(node.key.as_mut_ptr());
+            drop_in_place(node.val.as_mut_ptr());
+        });
+        // re-box the head/tail, and because these are maybe-uninit
+        // they do not have the absent k/v dropped
+        let _head = unsafe { *Box::from_raw(self.head) };
+        let _tail = unsafe { *Box::from_raw(self.tail) };
+    }
+}
+
+impl<K, V> Clone for LruCache<K, V>
+where
+    K: Hash + PartialEq + Eq + Clone,
+    V: Clone,
+{
+    fn clone(&self) -> Self {
+        let mut new_lru = LruCache::new(self.cap);
+        for (key, val) in self.iter().rev() {
+            new_lru.push(key.clone(), val.clone());
+        }
+        new_lru
+    }
+}
+
+impl<'a, K: Hash + Eq, V> IntoIterator for &'a LruCache<K, V> {
+    type Item = (&'a K, &'a V);
+    type IntoIter = Iter<'a, K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+// An iterator over the entries of a `LruCache`.
+pub struct Iter<'a, K: 'a, V: 'a> {
+    ptr: *const Node<K, V>,
+    end: *const Node<K, V>,
+    len: usize,
+    _phantom: PhantomData<&'a K>,
+}
+
+impl<'a, K, V> Iterator for Iter<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.len == 0 {
+            return None;
+        }
+        let key = unsafe { &*(*self.ptr).key.as_ptr() };
+        let val = unsafe { &*(*self.ptr).val.as_ptr() };
+        self.ptr = unsafe { (*self.ptr).next };
+        self.len -= 1;
+        Some((key, val))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+
+    fn count(self) -> usize {
+        self.len
+    }
+}
+
+impl<'a, K, V> DoubleEndedIterator for Iter<'a, K, V> {
+    fn next_back(&mut self) -> Option<(&'a K, &'a V)> {
+        if self.len == 0 {
+            return None;
+        }
+        let key = unsafe { &*(*self.end).key.as_ptr() };
+        let val = unsafe { &*(*self.end).val.as_ptr() };
+        self.ptr = unsafe { (*self.ptr).prev };
+        self.len -= 1;
+        Some((key, val))
+    }
+}
+
+impl<'a, K, V> Clone for Iter<'a, K, V> {
+    fn clone(&self) -> Iter<'a, K, V> {
+        Iter {
+            len: self.len,
+            ptr: self.ptr,
+            end: self.end,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a, K, V> ExactSizeIterator for Iter<'a, K, V> {}
+
+impl<'a, K, V> FusedIterator for Iter<'a, K, V> {}
+
+// The compiler does not automatically derive Send and Sync for Iter because it contains
+// raw pointers.
+unsafe impl<'a, K: Send, V: Send> Send for Iter<'a, K, V> {}
+
+unsafe impl<'a, K: Sync, V: Sync> Sync for Iter<'a, K, V> {}
+
+impl<'a, K: Hash + Eq, V> IntoIterator for &'a mut LruCache<K, V> {
+    type Item = (&'a K, &'a mut V);
+    type IntoIter = IterMut<'a, K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter_mut()
+    }
+}
+
+// An iterator over mutable entries of a `LruCache`.
+pub struct IterMut<'a, K: 'a, V: 'a> {
+    ptr: *mut Node<K, V>,
+    end: *mut Node<K, V>,
+    len: usize,
+    _phantom: PhantomData<&'a K>,
+}
+
+impl<'a, K, V> Iterator for IterMut<'a, K, V> {
+    type Item = (&'a K, &'a mut V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.len == 0 {
+            return None;
+        }
+        let key = unsafe { &*(*self.ptr).key.as_ptr() };
+        let val = unsafe { &mut *(*self.ptr).val.as_mut_ptr() };
+        self.ptr = unsafe { (*self.ptr).next };
+        self.len -= 1;
+        Some((key, val))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+
+    fn count(self) -> usize {
+        self.len
+    }
+}
+
+impl<'a, K, V> DoubleEndedIterator for IterMut<'a, K, V> {
+    fn next_back(&mut self) -> Option<(&'a K, &'a mut V)> {
+        if self.len == 0 {
+            return None;
+        }
+        let key = unsafe { &*(*self.end).key.as_ptr() };
+        let val = unsafe { &mut *(*self.end).val.as_mut_ptr() };
+        self.ptr = unsafe { (*self.ptr).prev };
+        self.len -= 1;
+        Some((key, val))
+    }
+}
+
+impl<'a, K, V> ExactSizeIterator for IterMut<'a, K, V> {}
+
+impl<'a, K, V> FusedIterator for IterMut<'a, K, V> {}
+
+// The compiler does not automatically derive Send and Sync for Iter because it contains
+// raw pointers.
+unsafe impl<'a, K: Send, V: Send> Send for IterMut<'a, K, V> {}
+
+unsafe impl<'a, K: Sync, V: Sync> Sync for IterMut<'a, K, V> {}
+
+impl<K: Hash + Eq, V> IntoIterator for LruCache<K, V> {
+    type Item = (K, V);
+    type IntoIter = IntoIter<K, V>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter { cache: self }
+    }
+}
+
+// An iterator that moves out of a `LruCache`.
+pub struct IntoIter<K: Hash + Eq, V> {
+    cache: LruCache<K, V>,
+}
+
+impl<K: Hash + Eq, V> Iterator for IntoIter<K, V> {
+    type Item = (K, V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.cache.pop_lru()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.cache.len();
+        (len, Some(len))
+    }
+
+    fn count(self) -> usize {
+        self.cache.len()
+    }
+}
+
+impl<K, V> ExactSizeIterator for IntoIter<K, V> where K: Hash + Eq {}
+
+impl<K, V> FusedIterator for IntoIter<K, V> where K: Hash + Eq {}
+
+// The compiler does not automatically derive Send and Sync for LruCache because it contains
+// raw pointers. The raw pointers are safely encapsulated by LruCache though so we can
+// implement Send and Sync for it below.
+unsafe impl<K: Send, V: Send> Send for LruCache<K, V> {}
+
+unsafe impl<K: Sync, V: Sync> Sync for LruCache<K, V> {}
+
+impl<K: Hash + Eq, V> fmt::Debug for LruCache<K, V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("LruCache")
+            .field("len", &self.len())
+            .field("cap", &self.cap())
+            .finish()
     }
 }
